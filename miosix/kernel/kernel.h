5f2d24a94b5c83121082010383e53a8b4ad9d13f
Sasan Golchin <ahmad.golchin@mail.polimi.it>
2016-05-26 16:15:33 +0200

    The hardcoded sleeping_list is replaced with IntrusiveList<SleepData>, IRQsetNextPreemption function is added to the kernel, The IRQFindNextThread of scheduler's interface and priority scheduler is amended to return the burst. TODO: Other schedulers should also have the same change in near future.

--- miosix/kernel/kernel.h
+++ miosix/kernel/kernel.h
@@ -35,6 +35,7 @@
 #include "kernel/scheduler/sched_types.h"
 #include "stdlib_integration/libc_integration.h"
 #include "stdlib_integration/libstdcpp_integration.h"
+#include "intrusive.h"
 #include <cstdlib>
 #include <new>
 #include <functional>
@@ -1059,7 +1060,7 @@ public:
  * This struct is used to make a list of sleeping threads.
  * It is used by the kernel, and should not be used by end users.
  */
-struct SleepData
+struct SleepData : public IntrusiveListItem
 {
     ///\internal Thread that is sleeping
     Thread *p;
@@ -1067,8 +1068,6 @@ struct SleepData
     ///\internal When this number becomes equal to the kernel tick,
     ///the thread will wake
     long long wakeup_time;
-    
-    SleepData *next;///<\internal Next thread in the list
 };
 
 /**
