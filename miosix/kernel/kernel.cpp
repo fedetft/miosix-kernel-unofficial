edb33fc3b94dd437e54e9ff5d8e143a296a640d4
Fabiano Riccardi <fabiuz4@hotmail.it>
2016-10-24 16:00:22 +0200

    Refactor of cstimer.h, renamed all the files that use its functions
    Signed-off-by: Fabiano Riccardi <fabiuz4@hotmail.it>

--- miosix/kernel/kernel.cpp
+++ miosix/kernel/kernel.cpp
@@ -242,7 +242,7 @@ bool isKernelRunning()
 
 long long getTime()
 {
-    return ContextSwitchTimer::instance().getCurrentTick();
+    return ContextSwitchTimer::instance().getCurrentTime();
 }
 
 /**
@@ -370,7 +370,7 @@ void Thread::nanoSleep(long long ns)
 {
     if(ns==0) return; //TODO: should be (ns &lt; resolution + epsilon)
     //TODO: Mutual Exclusion issue
-    nanoSleepUntil(ContextSwitchTimer::instance().getCurrentTick() + ns);
+    nanoSleepUntil(ContextSwitchTimer::instance().getCurrentTime() + ns);
 }
 
 void Thread::nanoSleepUntil(long long absoluteTime)
