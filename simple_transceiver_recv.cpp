c8bee9ee6330b3799a88b0063b2a441f7f7bbbc3
Fabiano Riccardi <fabiuz4@hotmail.it>
2016-10-31 12:25:29 +0100

    Now the simple test (send-recv) for Transceiver is working
    Signed-off-by: Fabiano Riccardi <fabiuz4@hotmail.it>

--- simple_transceiver_recv.cpp
+++ simple_transceiver_recv.cpp
@@ -6,24 +6,30 @@
 using namespace std;
 using namespace miosix;
 
+const static int N=100;
+
 int main()
 {
-	Transceiver& rtx=Transceiver::instance();
-	TransceiverConfiguration tc(2450); 
-	rtx.configure(tc);
-        char packet[100]={0};
-	for(;;)
-	{
-		ledOn();
-		puts("on");
-		rtx.turnOn();
-		
-                rtx.recv(packet,100,1000000000000LL);
-                
-		memDump(packet,100);
-		
-		ledOff();
-		puts("off");
-		rtx.turnOff();
-	}
+    printf("\tRECEIVER SLAVE\n");
+    Transceiver& rtx=Transceiver::instance();
+    TransceiverConfiguration tc(2450);
+    TransceiverTimer& tim=TransceiverTimer::instance();
+    RecvResult result;
+    rtx.configure(tc);
+    char packet[N]={0};
+    long long oldTimestamp=0;
+    for(;;){
+	ledOn();
+	puts("on");
+	rtx.turnOn();
+
+	result=rtx.recv(packet,N,tim.getValue()+1000000);
+	//memDump(packet,100);
+	
+	ledOff();
+	printf("%lld %d %d, diff=%lld off\n", result.timestamp,result.timestamp-oldTimestamp);
+	oldTimestamp=result.timestamp;
+	rtx.turnOff();
+	Thread::sleep(1);
+    }
 }
--- simple_transceiver_send.cpp
+++ simple_transceiver_send.cpp
@@ -7,7 +7,11 @@
 using namespace std;
 using namespace miosix;
 
+const static int N=100;
+const static long long timeInterval=48000000;
+
 int main(){
+    printf("\SENDER MASTER\n");
     Transceiver& rtx=Transceiver::instance();
     TransceiverConfiguration tc(2450); 
     rtx.configure(tc);
@@ -18,21 +22,18 @@ int main(){
     for(int i=0;i<N;i++){
 	packet[i]=i;
     }
-
-    for(;;){
+    long long oldTimestamp=0;
+    rtx.turnOn();
+    for(long long i=96000000;;i+=timeInterval){
 	ledOn();
-	puts("on");
-	rtx.turnOn();
 
 	//rtx.sendNow(packet,100);
-	rtx.sendAt(packet,100,tim.getValue()+4800000);
-	
-	Thread::sleep(20);
+	rtx.sendAt(packet,N,i);
 	ledOff();
-	puts("off");
-	rtx.turnOff();
 	
-	//NOTE: this sleep is very important, if we call turnOff and turnOn immediatly, the system stops
+	printf("Sent at:%lld\n",i);
+	//NOTE: this sleep is very important, if we call turnOff and turnOn immediately, the system stops
 	Thread::sleep(100);
     }
+    rtx.turnOff();
 }
