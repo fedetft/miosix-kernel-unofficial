21b51609fe88f88a4bdcf9beadc857b53c4ddb2c
Fabiano Riccardi <fabiuz4@hotmail.it>
2016-11-20 09:59:09 +0100

    Trial for correct the hrt
    Signed-off-by: Fabiano Riccardi <fabiuz4@hotmail.it>

--- sync_rtc_hft.cpp
+++ sync_rtc_hft.cpp
@@ -1,4 +1,3 @@
-
 #include <cstdio>
 #include <miosix.h>
 #include "interfaces-impl/transceiver_timer.h"
@@ -8,15 +7,56 @@
 
 using namespace miosix;
 
+int correction=0;
+
+void readRtc(void *x){
+    Rtc& rtc=Rtc::instance();
+    while(1){
+	GPIOtimer g=GPIOtimer::instance();
+	long long rtcTime,time1,hrtIdeal;
+	int a=0,b=0,d=0,e=0;
+        int prev;
+	{
+            FastInterruptDisableLock dLock;
+            prev=loopback32KHzIn::value();
+            a=RTC->CNT;
+            //Wait for descend
+            for(;;){
+                    int curr=loopback32KHzIn::value();
+                    if(curr==0 && prev==1) break;
+                    prev=curr;
+            }
+            d=TIMER2->CNT;
+            b=RTC->CNT;
+            TIMER2->CC[2].CTRL=TIMER_CC_CTRL_ICEDGE_RISING
+                                            | TIMER_CC_CTRL_FILT_DISABLE
+                                            | TIMER_CC_CTRL_INSEL_PIN
+                                            | TIMER_CC_CTRL_MODE_INPUTCAPTURE;
+            //Wait to raise
+            while((TIMER2->IF & TIMER_IF_CC2)==0) ;
+            TIMER2->CC[2].CTRL=0;
+            TIMER2->IFC=TIMER_IFC_CC2;
+            e=TIMER2->CC[2].CCV;
+            rtcTime=rtc.IRQgetValue();
+            time1=g.getValue();
+            time1=(time1 & 0xFFFFFFFFFFFF0000LL) | e;
+            hrtIdeal=rtcTime*48000000/32768;
+        }
+        printf("RTC: %lld, HRT: %lld, HRT ideal: %lld, diff: %lld\n",rtcTime,time1,hrtIdeal,time1-hrtIdeal);
+        Thread::sleep(3);
+    }
+}
+
 void test()
 {
 	Rtc& rtc=Rtc::instance();
 	GPIOtimer g=GPIOtimer::instance();
-	long long rtcTime,time1,time2;
+	long long rtcTime,time1,time2,t1,t2;
 	int a=0,b=0,c=0,d=0,e=0;
+        int prev;
 	{
 		FastInterruptDisableLock dLock;
-		int prev=loopback32KHzIn::value();
+		prev=loopback32KHzIn::value();
  		a=RTC->CNT;
 		//Wait for descend
 		for(;;)
@@ -36,18 +76,18 @@ void test()
 		TIMER2->CC[2].CTRL=0;
 		TIMER2->IFC=TIMER_IFC_CC2;
 		e=TIMER2->CC[2].CCV;
-		rtcTime=rtc.IRQgetValue();
- 		c=RTC->CNT;
+		c=rtc.IRQgetValue();
 		time1=g.getValue();
-	}
-	printf("a=%d b=%d c=%d d=%d e=%x %llx e-d=%d\n",a,b,c,d,e,time1,e>=d ? e-d : e+65536-d);
+	
+        t1=c;
+	//printf("a=%d b=%d c=%d d=%d e=%x %llx e-d=%d\n",a,b,c,d,e,time1,e>=d ? e-d : e+65536-d);
 	time1=(time1 & 0xFFFFFFFFFFFF0000LL) | e;
 	
-	Thread::sleep(30000);
+	delayMs(15000);
 	
-	{
-		FastInterruptDisableLock dLock;
-		int prev=loopback32KHzIn::value();
+	
+		//FastInterruptDisableLock dLock;
+		prev=loopback32KHzIn::value();
  		a=RTC->CNT;
 		//Wait for descend
 		for(;;)
@@ -67,24 +107,20 @@ void test()
 		TIMER2->CC[2].CTRL=0;
 		TIMER2->IFC=TIMER_IFC_CC2;
 		e=TIMER2->CC[2].CCV;
-		rtcTime=rtc.IRQgetValue();
- 		c=RTC->CNT;
+		c=rtc.IRQgetValue();
 		time2=g.getValue();
 	}
-	printf("a=%d b=%d c=%d d=%d e=%x %llx e-d=%d\n",a,b,c,d,e,time2,e>=d ? e-d : e+65536-d);
+        t2=c;
+	//printf("a=%d b=%d c=%d d=%d e=%x %llx e-d=%d\n",a,b,c,d,e,time2,e>=d ? e-d : e+65536-d);
 	time2=(time2 & 0xFFFFFFFFFFFF0000LL) | e;
-	printf("%llx %llx %llx\n\n",time1,time2,time2-time1);
+	printf("%llx %llx\n\n",time2-time1,t2-t1);
 }
 
-int main()
-{
-	initDebugPins();
-		
-	for(;;)
-	{
-		puts("----");
-		test();
-		
-		Thread::sleep(2000);
-	}
+int main(){
+    initDebugPins();
+    //Thread::create(readRtc,2048,0,0);
+    for(;;){
+        test();
+        Thread::sleep(2000);
+    }
 }
