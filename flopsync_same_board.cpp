34976807b5da453737ab7c5787ea7b8750a14a9e
Fabiano Riccardi <fabiuz4@hotmail.it>
2016-12-19 12:14:36 +0100

    Corrected a bug in rtc, now if we read from gpio_timer andradio timer we get coherent values (like the timers start at th same time). There is still a bug in case we read the gpiotimer before  the first synchronization
    Signed-off-by: Fabiano Riccardi <fabiuz4@hotmail.it>

--- flopsync_same_board.cpp
+++ flopsync_same_board.cpp
@@ -12,6 +12,9 @@ using namespace miosix;
  * This code aim to sync the 2 clock on wandstem. The master is HFXO and the slave is LFXO, the interesting part of code is in high_resolution_timer_base
  */
 
+GPIOtimer* g; 
+Rtc* rtc;
+
 void task1(void* ){
     HighResolutionTimerBase::tWaiting=Thread::getCurrentThread();
     Flopsync1 f;
@@ -19,25 +22,33 @@ void task1(void* ){
 	Thread::wait();
 	pair<int,int> result=f.computeCorrection(HighResolutionTimerBase::error);
 	HighResolutionTimerBase::clockCorrection=result.first;
-	//printf("Timestamped=%lld Expected=%lld e=%lld %lld\n",HighResolutionTimerBase::syncPointHrtTimestamped,HighResolutionTimerBase::syncPointHrtExpected,HighResolutionTimerBase::error,HighResolutionTimerBase::clockCorrection);
-	printf("%lld\n",HighResolutionTimerBase::error);
+	//printf("Timestamped=%lld Expected=%lld e=%lld %lld\n",HighResolutionTimerBase::syncPointHrtSlave,HighResolutionTimerBase::syncPointHrtMaster,HighResolutionTimerBase::error,HighResolutionTimerBase::clockCorrection);
+	//printf("%lld\n",HighResolutionTimerBase::error);
 	//greenLed::toggle();
     }
 }
-int a=0;
+
 void task2(void* ){
     for(;;){
-	delayMs(1000);
-	Thread::sleep(200);
+	//delayMs(1000);
+	long long a=g->getValue();
+	//long long b=rtc->getValue();  
+	//printf("HRT:%lld RTC:%lld conv:%lld diff:%lld\n", a,b,b*48000000/32768,a-b*48000000/32768);
+	printf("%lld\n",a);
+	//Thread::sleep(200);
 	
     }
 }
 
-void task3(void* ){
-    for(;;){	
+void task3(void*){
+    for(;;){
 	greenLed::toggle();
 	Thread::sleep(20);
     }
+}
+
+void task0(void*){
+    HighResolutionTimerBase::queue.run();
 }
 /*
 long long rtcTime,time1,hrtIdeal;
@@ -59,13 +70,15 @@ long long rtcTime,time1,hrtIdeal;
 	    delayMs(1);
 	}
 */
+
 int main(int argc, char** argv) {
     printf("\t\tflopsync_same_board\n");
     
+    rtc=&Rtc::instance();
+    g=&GPIOtimer::instance();
+    
     //Configuration of timers
     PRS->CH[4].CTRL= PRS_CH_CTRL_SOURCESEL_RTC | PRS_CH_CTRL_SIGSEL_RTCCOMP1; 
-    Rtc& rtc=Rtc::instance();
-
     TIMER2->CC[2].CTRL=TIMER_CC_CTRL_ICEDGE_RISING
 			| TIMER_CC_CTRL_FILT_DISABLE
 			| TIMER_CC_CTRL_INSEL_PRS
@@ -73,17 +86,17 @@ int main(int argc, char** argv) {
 			| TIMER_CC_CTRL_MODE_INPUTCAPTURE;
     
     //printf("Inizio %lu\n",RTC->CNT);
-    RTC->COMP1=2999;
+    RTC->COMP1=3000-1;
     RTC->IFC=RTC_IFC_COMP1;
-    RTC->IEN=RTC_IEN_COMP1;
+    RTC->IEN|=RTC_IEN_COMP1;
     
     TIMER2->IEN |= TIMER_IEN_CC2;
     while(RTC->SYNCBUSY & RTC_SYNCBUSY_COMP1) ;
-    
+    //Thread::create(task0,2048,2);
     Thread::create(task1,2048,3);
     Thread::create(task2,2048,1);
     Thread::sleep(10);
-    Thread::create(task3,2048,0);
+    //Thread::create(task3,2048,0);
     Thread::wait();
     
     return 0;
