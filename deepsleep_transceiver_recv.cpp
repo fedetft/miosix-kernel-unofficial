72e958cd9035101d234494d622094bb322eb3389
Fabiano Riccardi <fabiuz4@hotmail.it>
2017-02-28 15:51:19 +0100

    The radio is working with VHT
    Signed-off-by: Fabiano Riccardi <fabiuz4@hotmail.it>

---
+++ deepsleep_transceiver_recv.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "miosix.h"
+#include "interfaces-impl/transceiver.h"
+#include "interfaces-impl/power_manager.h"
+#include "interfaces-impl/rtc.h"
+
+using namespace std;
+using namespace miosix;
+
+const int n=10;
+const int packetLen=1;
+
+/*
+ * Send some packet, while deepsleep for a while and then continue to send packets
+ */
+int main(int argc, char** argv) {
+    printf("Receiver in Deep Sleep test\n\n");
+    char packet[packetLen];
+    long long last=-1;
+    PowerManager& pw=PowerManager::instance();
+    Rtc& rtc=Rtc::instance();
+    Transceiver& t=Transceiver::instance();
+    
+    t.turnOn();
+    for(;;){
+        RecvResult result=t.recv(packet,packetLen,1000000000,Transceiver::Unit::TICK);
+        if(packet[0]==2){
+            pw.deepSleepUntil(rtc.getValue()+32768*2);
+            last=-1;
+        }else{
+            if(result.timestampValid&&result.error==RecvResult::ErrorCode::OK){
+                if(last!=-1){
+                    printf("%lld\n",result.timestamp-last);
+                }
+                last=result.timestamp;
+            }else{
+                printf("Error timestamp\n");
+            }
+        }
+    }
+    t.turnOff();
+    return 0;
+}
---
+++ deepsleep_transceiver_send.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "miosix.h"
+#include "interfaces-impl/transceiver.h"
+#include "interfaces-impl/power_manager.h"
+#include "interfaces-impl/rtc.h"
+#include "interfaces-impl/transceiver_timer.h"
+
+
+using namespace std;
+using namespace miosix;
+
+const int n=10;
+const int packetLen=1;
+const int interval=4800000;   ///< in tick 
+/*
+ * Send some packet, while deepsleep for a while and then continue to send packets
+ */
+int main(int argc, char** argv) {
+    printf("Sender in deep sleep test\n\n");
+    char packet[packetLen];
+    Transceiver& t=Transceiver::instance();
+    TransceiverTimer& tt=TransceiverTimer::instance();
+    PowerManager& pw=PowerManager::instance();
+    Rtc& rtc=Rtc::instance();
+    int i=0;
+    
+    t.turnOn();
+    for(long long time=96000000;;time+=interval){
+        
+        if(i<10){
+            packet[0]=0;
+        }else if(i==10){
+            packet[0]=2;
+        }else{
+            i=-1;
+        }
+        if(i!=-1){
+            printf("Send #%i\n",i);
+            t.sendAt(packet,packetLen,time,Transceiver::Unit::TICK);
+        }else{
+            printf("Deep sleep...");
+            pw.deepSleepUntil(rtc.getValue()+32768*2);
+            printf("Wake up at %lld\n",getTime());
+            
+            //Delay to give the possibility to the receiver to wake up
+            Thread::sleep(100);
+            time=tt.getValue();
+        }
+        i++;
+    }
+    t.turnOff();
+    return 0;
+}
+
