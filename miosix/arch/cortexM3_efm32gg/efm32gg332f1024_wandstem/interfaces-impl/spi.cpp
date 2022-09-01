25a6cbd7f36f99c3b6a3328d546369f071a79b2c
Alessandro Sorrentino <sorre97@hotmail.it>
2022-09-01 17:41:28 +0200

    Fixed SPI import

--- miosix/arch/cortexM3_efm32gg/efm32gg332f1024_wandstem/interfaces-impl/spi.cpp
+++ miosix/arch/cortexM3_efm32gg/efm32gg332f1024_wandstem/interfaces-impl/spi.cpp
@@ -25,7 +25,7 @@
  *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
  ***************************************************************************/
 
-#include "interfaces/spi.h"
+#include "spi.h"
 #include <miosix.h>
 
 using namespace std;
